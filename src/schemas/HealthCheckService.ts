887c5f0ae9be8519e6b7e152d90d3afc37c880b6
yopiidev <yopiidev@gmail.com>
2023-05-19 14:11:42 +0900

    update docs config

--- src/schemas/HealthCheckService.ts
+++ src/schemas/HealthCheckService.ts
@@ -7,8 +7,9 @@ import { HealthCheckRegistration } from './HealthCheckRegistration'
 export interface HealthCheckService {
   /**
    * Runs the provided health checks and returns the aggregated status.
-   * @param predicate? A predicate that can be used to include health checks based on user-defined criteria.
-   * @param timeoutMilliseconds? A milliseconds which can be used to cancel the health checks.
+   * @param predicate A predicate that can be used to include health checks based on user-defined criteria.
+   * @param timeoutMilliseconds A milliseconds which can be used to cancel the health checks.
+   * @return A HealthReport instance.
    */
   checkHealthAsync(
     predicate?: (registration: HealthCheckRegistration) => boolean,
